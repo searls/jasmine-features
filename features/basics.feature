980ec4b3db688b6f89f2504a83c46b69ade91363
Justin Searls <searls@gmail.com>
2012-02-06 11:25:58 -0500

    added a cuke that knocks jQuery out of orbit and then ensures jasmine-features is still working after being configured to use a different instance of it.

--- features/basics.feature
+++ features/basics.feature
@@ -6,3 +6,8 @@ Feature:
   Scenario: client-side functional tests
     When I run all client-side functional tests
     Then I see no client-side functional failures
+
+  Scenario: custom jQuery
+    Given I configure jasmine-features to use a different jQuery
+    When I run all client-side functional tests
+    Then I see no client-side functional failures
--- features/step_definitions/functional_steps.rb
+++ features/step_definitions/functional_steps.rb
@@ -4,6 +4,11 @@ Given /^I open the fixture page$/ do
   page.should have_content "Loaded!"
 end
 
+Given /^I configure jasmine-features to use a different jQuery$/ do
+  page.execute_script("window.jQuery.fn = 'borked jquery'")
+  page.execute_script("window.jasmine.features.using({jQuery: window.jQuery_1_6_4})")
+end
+
 When /^I run all client\-side functional tests$/ do
   page.execute_script("window.jasmine.features.run()")
 end
